66df971d13540e265d59c636136a72b309490b4b
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-13 14:31:49 -0300

    ADD main script 'CadastroProduto'

--- src/main/java/br/com/matheus/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/matheus/loja/testes/CadastroDeProduto.java
@@ -1,4 +1,26 @@
 package br.com.matheus.loja.testes;
 
+import br.com.matheus.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
 public class CadastroDeProduto {
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi RedMi");
+        celular.setDescricao("Celular muito legal");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManagerFactory factory = Persistence.
+                createEntityManagerFactory("loja");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
 }
