20facbe8445cacf0fb72f22165556e3a38fb3500
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-13 23:18:28 -0300

    Consultas com filtros - by namedParameter

--- src/main/java/br/com/matheus/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/matheus/loja/dao/ProdutoDao.java
@@ -24,4 +24,10 @@ public class ProdutoDao {
         String jpql = "SELECT p FROM Produto p";
         return em.createQuery(jpql, Produto.class).getResultList();
     }
+    public List<Produto> buscarPorNome(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome ";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
 }
--- src/main/java/br/com/matheus/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/matheus/loja/testes/CadastroDeProduto.java
@@ -20,7 +20,7 @@ public class CadastroDeProduto {
         Produto p = produtoDao.buscarPorId(1L);
         System.out.println(p.getPreco());
 
-        List<Produto> todos = produtoDao.buscarTodos();
+        List<Produto> todos = produtoDao.buscarPorNome("Xiaomi RedMi");
         todos.forEach(p2 -> System.out.println(p.getNome()));
 
     }
