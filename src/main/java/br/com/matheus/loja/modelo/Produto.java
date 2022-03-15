6799a682768e3cd965e6a5b1e7316a3ab94faba9
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-15 10:23:13 -0300

    Mapeamento de herança JOINED

--- src/main/java/br/com/matheus/loja/modelo/Produto.java
+++ src/main/java/br/com/matheus/loja/modelo/Produto.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 @Table(name = "produtos")
 @NamedQuery(name= "Produto.produtosPorCategoria",
         query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome ")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Produto {
 
     @Id
