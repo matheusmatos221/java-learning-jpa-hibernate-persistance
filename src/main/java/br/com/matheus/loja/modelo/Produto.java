92f09cfad48125c0515332a9fd16eb426b186fd9
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-15 10:20:18 -0300

    Mapeamento de herança SingleTable

--- src/main/java/br/com/matheus/loja/modelo/Produto.java
+++ src/main/java/br/com/matheus/loja/modelo/Produto.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 @Table(name = "produtos")
 @NamedQuery(name= "Produto.produtosPorCategoria",
         query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome ")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Produto {
 
     @Id
